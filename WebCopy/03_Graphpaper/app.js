7c8d160b5f283e190569dd253e3ae4e2e9857276
nino <god19124@gmail.com>
2024-01-24 00:37:23 +0900

    [2024/01/24] ■03_Graphpaper
    ■変更内容
    ・subImageもズームから縮小されるように実装

--- WebCopy/03_Graphpaper/app.js
+++ WebCopy/03_Graphpaper/app.js
@@ -2,10 +2,7 @@ let mainImage = document.querySelector(".carousel-item");
 
 //ある特定の位置を超えると作動する関数
 let setItemActive = (entries) => {
-    console.log(entries);
-
     entries.map((entry) => {
-
         if(entry.isIntersecting){
             entry.target.classList.add("active-1");
         }
@@ -18,3 +15,19 @@ let options = {};
 let observer = new IntersectionObserver(setItemActive, options);
 
 observer.observe(mainImage);
+
+let subImage = [...document.querySelectorAll(".image")];
+
+let setImageActive = (entires) => {
+    entires.map((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("active-1");
+        }
+    });
+};
+
+let observer1 = new IntersectionObserver(setImageActive, options);
+
+subImage.map((image) => {
+    observer1.observe(image);
+})
