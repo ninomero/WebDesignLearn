5760f515e16fe2e91834cfc0aae9a1529c7e1a9c
nino <god19124@gmail.com>
2024-01-24 00:46:48 +0900

    [2024/01/24] ■03_Graphpaper
    ■変更内容
    ・コメント、フォーマット整形整形

--- WebCopy/03_Graphpaper/app.js
+++ WebCopy/03_Graphpaper/app.js
@@ -1,33 +1,26 @@
 let mainImage = document.querySelector(".carousel-item");
+let subImage = [...document.querySelectorAll(".image")];
 
 //ある特定の位置を超えると作動する関数
 let setItemActive = (entries) => {
-    entries.map((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add("active-1");
-        }
-    });
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active-1");
+    }
+  });
 };
 
-let options = {};
+//具体的にいつ発動させるのかを決めるオプション
+let options = {
+  rootMargin: "0px", //デフォルトで０.marginとほぼ同じ。
+  threshold: 0.5, //閾値は0.2。これが１になると完全に画面におさまってから発動する
+};
 
 //監視
 let observer = new IntersectionObserver(setItemActive, options);
-
 observer.observe(mainImage);
 
-let subImage = [...document.querySelectorAll(".image")];
-
-let setImageActive = (entires) => {
-    entires.map((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add("active-1");
-        }
-    });
-};
-
-let observer1 = new IntersectionObserver(setImageActive, options);
-
+//複数の.imageが読み込まれるたびに.active-1を付与する
 subImage.map((image) => {
-    observer1.observe(image);
-})
+    observer.observe(image);
+});
